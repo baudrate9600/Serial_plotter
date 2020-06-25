7cc5aed98ab5cccf37861a57c8d8e5813bb120b7
Olasoji <olasojimakinwa@gmail.com>
2020-06-25 16:19:38 +0200

    Updated the serial read function
    Now it can read data from the serial port
    
    It returns an array with the data and the amount of
    bytes.

--- src/main.py
+++ src/main.py
@@ -6,7 +6,11 @@ ser.conf_port(baudrate=9600)
 ser.set_timeouts()
 
 while 1 > 0 :
-    test = ser.read()
-    print(test)
-    print (repr(ctypes.cast(test, ctypes.c_char_p).value))
+    s,num = ser.read()
+    #print(s)
+    #print(num)
+    if num > 0:
+        #print (repr(ctypes.cast(s, ctypes.c_char_p).value))
+        print(num,s.raw)
+
 
