39c2aca8ef7ad8f2cf3b8a2136ffdcf78b53a9d9
Olasoji <olasojimakinwa@gmail.com>
2020-06-26 22:49:52 +0200

    Made a start to the gui
    Also added a nice (trash) png for the application icon

--- src/main.py
+++ src/main.py
@@ -1,16 +1,40 @@
+#Serial module 
 import serial as ser 
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+from PyQt5.QtGui import QIcon 
 import ctypes 
 
 ser.open_port("COM4")
 ser.conf_port(baudrate=9600) 
 ser.set_timeouts()
 
-while 1 > 0 :
-    s,num = ser.read()
-    #print(s)
-    #print(num)
-    if num > 0:
-        #print (repr(ctypes.cast(s, ctypes.c_char_p).value))
-        print(num,s.raw)
+x = np.linspace(0,2,100)
+y = np.linspace(0,2,100)
+
+#QT5 
+matplotlib.use('Qt5Agg')
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI() 
+
+    def initUI(self): 
+        self.setGeometry(300, 300, 300, 220) 
+        self.setWindowTitle('Icon') 
+        self.setWindowIcon(QIcon('..\lib\serial.png'))
+        self.show()
+
+def main():
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
 
 
+if __name__ == '__main__':
+    main()
